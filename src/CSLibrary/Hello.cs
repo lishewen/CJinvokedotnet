f3521b446a2cb1d71fb7a00020167c374609be79
算神 <lishewen@msn.com>
2025-07-02 18:40:22 +0800

    仓颉调用.net9 C# dll

--- src/CSLibrary/Hello.cs
+++ src/CSLibrary/Hello.cs
@@ -1,9 +1,17 @@
-﻿namespace CSLibrary;
+﻿using System.Runtime.InteropServices;
+namespace CSLibrary;
 
-public class Hello
+public static class Hello
 {
-    public string SayHello(string name)
+    [UnmanagedCallersOnly(EntryPoint = "sayhello")]
+    public static IntPtr SayHello(IntPtr namePtr)
     {
-        return $"Hello, {name}! My name is CSLibrary.Hello";
+        // 将非托管字符串指针转换为C#字符串
+        string? name = Marshal.PtrToStringUTF8(namePtr);
+        string greeting = $"Hello, {name}! My name is CSLibrary.Hello";
+
+        // 将C#字符串转换为非托管UTF-8字符串指针
+        IntPtr resultPtr = Marshal.StringToCoTaskMemUTF8(greeting);
+        return resultPtr;
     }
 }
