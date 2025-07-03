2bd85263f70f893539e812d15cdd19c61af241d9
算神 <lishewen@msn.com>
2025-07-03 11:56:53 +0800

    C#调用仓颉的DLL

--- src/CSLibrary/Hello.cs
+++ src/CSLibrary/Hello.cs
@@ -6,6 +6,9 @@ public static class Hello
     [UnmanagedCallersOnly(EntryPoint = "sayhello")]
     public static IntPtr SayHello(IntPtr namePtr)
     {
+        // 调用仓颉自定义库中的myHello方法
+        myHello();
+
         // 将非托管字符串指针转换为C#字符串
         string? name = Marshal.PtrToStringUTF8(namePtr);
         string greeting = $"你好, {name}！来自.NET 9的问候";
@@ -24,4 +27,8 @@ public static class Hello
             Marshal.FreeCoTaskMem(ptr);
         }
     }
+
+    // 导入仓颉库的自定义方法
+    [DllImport("libCJinvokedotnet.dll", EntryPoint = "myHello")]
+    private static extern void myHello();
 }
--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,4 @@
 @echo off
+cjc mylib.cj --output-type=dylib
 dotnet publish .\CSLibrary\CSLibrary.csproj -c Release -r win-x64 -o .
 cjc -L . -l CSLibrary ./main.cj
