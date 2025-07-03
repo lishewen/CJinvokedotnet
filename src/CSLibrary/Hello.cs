929b7c52badaac4324f9d9b17be1654199630ba0
算神 <lishewen@msn.com>
2025-07-03 11:14:19 +0800

    解决潜在的内存泄露问题

--- src/CSLibrary/Hello.cs
+++ src/CSLibrary/Hello.cs
@@ -8,10 +8,20 @@ public static class Hello
     {
         // 将非托管字符串指针转换为C#字符串
         string? name = Marshal.PtrToStringUTF8(namePtr);
-        string greeting = $"Hello, {name}! My name is CSLibrary.Hello";
+        string greeting = $"你好, {name}！来自.NET 9的问候";
 
         // 将C#字符串转换为非托管UTF-8字符串指针
         IntPtr resultPtr = Marshal.StringToCoTaskMemUTF8(greeting);
         return resultPtr;
+    }
+
+    // 添加内存释放函数供仓颉调用
+    [UnmanagedCallersOnly(EntryPoint = "free_string")]
+    public static void FreeString(IntPtr ptr)
+    {
+        if (ptr != IntPtr.Zero)
+        {
+            Marshal.FreeCoTaskMem(ptr);
+        }
     }
 }
